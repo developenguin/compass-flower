60046839fd08ad386bbfe6c11f6bd1bf4ddd24a1
developenguin <developenguin@users.noreply.github.com>
2019-11-08 15:02:52 +0100

    Draw only unique circles, static 2 layers for now

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const canvas = document.getElementById('app'),
       ctx = canvas.getContext('2d'),
       pointsInput = document.getElementById('points');
 
+class Circle {
+
+  constructor(props) {
+    this.x = props.x;
+    this.y = props.y;
+    this.r = props.r;
+  }
+
+  isSameAs(circle = {}) {
+
+    const thisX = parseFloat(this.x.toFixed(6)),
+          thisY = parseFloat(this.y.toFixed(6)),
+          otherX = parseFloat(circle.x.toFixed(6)),
+          otherY = parseFloat(circle.y.toFixed(6));
+
+    return thisX === otherX && thisY === otherY && this.r === circle.r
+
+  }
+
+  getCenter() {
+    return {
+      x: this.x,
+      y: this.y
+    }
+  }
+
+}
+
 function init() {
 
   redrawCanvas();
@@ -13,15 +41,7 @@ function registerEventListeners() {
 
   pointsInput.addEventListener('input', () => redrawCanvas());
 
-  onresize = () => redrawCanvas();
-
-}
-
-function redrawCanvas() {
-
-  const n = getNValue();
-
-  resetCanvas();
+  onresize = () => resetCanvas();
 
 }
 
@@ -51,49 +71,56 @@ function resetCanvas() {
 
 function drawFigure(center) {
 
-  const points = getPointsForN(getNValue(), center);
+  const n = getNValue();
+  const layer1 = getCirclesForN(n, center);
+  const layer2 = layer1
+    .map(circle => getCirclesForN(n, circle.getCenter()))
+    .flat();
 
-  points.forEach(point => {
-    drawFlower(point);
+  const flattened = [...layer1, ...layer2];
+
+  const filtered = flattened.reduce((acc, nextCircle) => {
+
+    const matches = acc.filter(circle => circle.isSameAs(nextCircle));
+
+    if (!matches.length) {
+      acc = [...acc, nextCircle];
+    }
+
+    return acc;
+
+  }, []);
+
+  filtered.forEach(circle => {
+    drawCircle(circle);
   });
 
 }
 
-function drawFlower(center) {
+function getCirclesForN(n, center) {
 
-  const points = getPointsForN(getNValue(), center);
-
-  points.forEach(point => {
-    drawCircle(point);
-  });
-
-}
-
-function getPointsForN(n, center) {
-
-  const points = [],
+  const circles = [],
     segmentAngleRadian = 2 * Math.PI / n,
     circleRadius = getCircleRadius();
 
   for (let i = 1; i <= n; i++) {
-    points.push({
+    circles.push(new Circle({
       x: center.x + circleRadius * Math.cos(i * segmentAngleRadian),
-      y: center.y + circleRadius * Math.sin(i * segmentAngleRadian)
-    });
+      y: center.y + circleRadius * Math.sin(i * segmentAngleRadian),
+      r: circleRadius
+    }));
   }
 
-  return points;
+  return circles;
 
 }
 
-function drawCircle(center) {
-
-  const radius = getCircleRadius();
+function drawCircle(circle) {
 
   ctx.strokeStyle = 'white';
   ctx.lineWidth = 2;
   ctx.beginPath();
-  ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+  ctx.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI);
   ctx.stroke();
 
 }
